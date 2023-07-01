554c108e252d2fe47c00e3bc03aae13328f96ec8
Pravindu <pravindubh@gmail.com>
2023-07-01 12:37:09 +0530

    After finishing the hero page scroll down css

---
+++ src/components/Heroimage.js
@@ -0,0 +1,24 @@
+import "./HeroimageStyle.css";
+import React from 'react'
+import IntroImg from "../assets/programmer.jpg";
+import { Link } from "react-router-dom";
+
+const Heroimage = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            <img className="intro-img" src={IntroImg} alt="Introimg"/> 
+        </div>
+        <div className="content">
+            <p>Hi, I am an IT undergratuate of University of Moratuwa</p>
+            <h1>Full Stack Developer</h1>
+            <div>
+            <Link to="/project" className="btn">Projects</Link>
+            <Link to="/contact" className="btn btn-light">Contact</Link>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Heroimage;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Heroimage from "../components/Heroimage";
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <Heroimage/>
     </div>
   );
 };
