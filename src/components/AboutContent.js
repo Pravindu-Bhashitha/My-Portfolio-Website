87c18bdc68f2fa17bfdf1468bfe52b8ef799efeb
Pravindu <pravindubh@gmail.com>
2023-07-02 16:49:05 +0530

    After completing the about page

---
+++ src/components/AboutContent.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./AboutContentStyle.css";
+import { Link } from "react-router-dom";
+import java from "../assets/Java.png";
+import python from "../assets/Python.png";
+import react from "../assets/React.jpg";
+import SQL from "../assets/SQLN.png";
+import NET from "../assets/NETN.png";
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+      <div className="left">
+        <h1>Who Am I?</h1>
+        <p style={{marginBottom:"10px"}}>
+          I am a dedicated, self-driven, and enthusiastic student striving to
+          contribute to the betterment of humanity.
+        </p>
+        <Link to="/contact">
+          <button className="btn">Contact</button>
+        </Link>
+        <div className="right">
+          <h1>Technologies I am good at...</h1>
+          <div className="img-container">
+            <img src={java} alt="javaimage" style={{width:"100px",height:"80px",margin:"0 20px 0 0"}}></img>
+            <img src={python} alt="pythonimage" style={{width:"80px",height:"80px",margin:"0 20px 0 0"}}></img>
+            <img src={react} alt="reactimage" style={{width:"100px",height:"80px",margin:"0 20px 0 0"}}></img>
+            <img src={SQL} alt="sqlimage" style={{width:"100px",height:"80px",margin:"0 20px 0 -25px"}}></img>
+            <img src={NET} alt="sqlimage" style={{width:"100px",height:"80px",margin:"0 20px 0 -25px"}}></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutContent;
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Heroimage2 from '../components/Heroimage2';
+import AboutContent from '../components/AboutContent';
 
 const About = () => {
   return (
     <div>
       <Navbar/>
       <Heroimage2 heading="ABOUT" text="I am a full stack developer who is currently reading for my BSc.(Hons) information technology degree in university of Moratuwa."/>
+      <AboutContent/>
       <Footer/>
     </div>
   )
