f80b238d2c2858a127df5d2da29ef57a1524a280
Pravindu <pravindubh@gmail.com>
2023-07-03 13:50:41 +0530

    Finished the Portfolio

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -6,29 +6,65 @@ import python from "../assets/Python.png";
 import react from "../assets/React.jpg";
 import SQL from "../assets/SQLN.png";
 import NET from "../assets/NETN.png";
-// import ME from "../assets/Me.jpg";
+// import CV from "../assets/YourCV.pdf";
 
 const AboutContent = () => {
+  const handleDownloadCV = () => {
+    // Create a virtual link element
+    const link = document.createElement("a");
+    link.href = "https://drive.google.com/file/d/1cudcTi2rJJajoil6tnuYfjilUIEa9MLH/view?usp=sharing"; // URL of your CV file
+    link.download = "Pravindu_Bhashitha.pdf"; // The default name for the downloaded file
+    link.target = "_blank";
+    link.click();
+  };
+
   return (
     <div className="about">
       <div className="left">
         {/* <img src={ME} alt="ProfilePhoto" className="pro-photo"></img> */}
         <h1>Who Am I?</h1>
-        <p style={{marginBottom:"10px"}}>
+        <p style={{ marginBottom: "10px" }}>
           I am a dedicated, self-driven, and enthusiastic student striving to
           contribute to the betterment of humanity.
         </p>
+        <button
+          className="btn"
+          style={{ width: "60%", marginLeft: "20%", marginBottom: "5%" }}
+          onClick={handleDownloadCV}
+        >
+          Download CV
+        </button>
         <Link to="/contact">
           <button className="btn">Contact</button>
         </Link>
         <div className="right">
           <h1>Technologies I am good at...</h1>
           <div className="img-container">
-            <img src={java} alt="javaimage" style={{width:"100px",height:"80px",margin:"0 20px 0 0"}}></img>
-            <img src={python} alt="pythonimage" style={{width:"80px",height:"80px",margin:"0 20px 0 0"}}></img>
-            <img src={react} alt="reactimage" style={{width:"100px",height:"80px",margin:"0 20px 0 0"}}></img>
-            <img src={SQL} alt="sqlimage" style={{width:"100px",height:"80px",margin:"0 20px 0 -25px"}}></img>
-            <img src={NET} alt="sqlimage" style={{width:"100px",height:"80px",margin:"0 20px 0 -25px"}}></img>
+            <img
+              src={java}
+              alt="javaimage"
+              style={{ width: "100px", height: "80px", margin: "0 20px 0 0" }}
+            ></img>
+            <img
+              src={python}
+              alt="pythonimage"
+              style={{ width: "80px", height: "80px", margin: "0 20px 0 0" }}
+            ></img>
+            <img
+              src={react}
+              alt="reactimage"
+              style={{ width: "100px", height: "80px", margin: "0 20px 0 0" }}
+            ></img>
+            <img
+              src={SQL}
+              alt="sqlimage"
+              style={{ width: "100px", height: "80px", margin: "0 20px 0 -25px" }}
+            ></img>
+            <img
+              src={NET}
+              alt="sqlimage"
+              style={{ width: "100px", height: "80px", margin: "0 20px 0 -25px" }}
+            ></img>
           </div>
         </div>
       </div>
