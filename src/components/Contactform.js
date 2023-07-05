0e46d9ebf1c20935573c21d7bb1bf8d85d353354
Pravindu <pravindubh@gmail.com>
2023-07-06 01:07:51 +0530

    Add contact form

--- src/components/Contactform.js
+++ src/components/Contactform.js
@@ -9,18 +9,19 @@ const ContactForm = () => {
   console.log(name,email,subject,message);
   return (
     <div className="form">
-      <form name="contact" method="POST">
+      <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
         <input type="hidden" name="form-name" value="contact" />
         <label>Your Name</label>
         <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} required />
         <label>Email</label>
-        <input type="email"id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
         <label>Subject</label>
         <input type="text" id="subject" name="subject" value={subject} onChange={(e) => setSubject(e.target.value)} required />
         <label>Message</label>
         <textarea
           rows="6"
           placeholder="Type Your Message Here"
+          id="message"
           name="message"
           value={message} onChange={(e) => setMessage(e.target.value)}
         />
