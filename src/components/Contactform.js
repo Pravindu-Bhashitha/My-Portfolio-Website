607b66483c71da7f5f4309dbdb0f37589d22e7ac
Pravindu <pravindubh@gmail.com>
2023-07-06 00:12:47 +0530

    Add contact form

--- src/components/Contactform.js
+++ src/components/Contactform.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FormStyles.css";
 
 const ContactForm = () => {
+  const [name,setName] = useState("");
+  const [message,setMessage] = useState("");
+  const [email,setEmail] = useState("");
+  const [subject,setSubject] = useState("");
   return (
     <div className="form">
       <form name="contact" method="POST">
-        <input type="hidden" name="form-name" value="contact"/>
+        {/* <input type="hidden" name="form-name" /> */}
         <label>Your Name</label>
-        <input type="text" id="name" name="name" required />
+        <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} required />
         <label>Email</label>
-        <input type="email"id="email" name="email" required />
+        <input type="email"id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
         <label>Subject</label>
-        <input type="text" id="subject" name="subject" required />
+        <input type="text" id="subject" name="subject" value={subject} onChange={(e) => setSubject(e.target.value)} required />
         <label>Message</label>
         <textarea
           rows="6"
           placeholder="Type Your Message Here"
           name="message"
+          value={message} onChange={(e) => setMessage(e.target.value)}
         />
         <button className="btn" type="submit">
           Submit
