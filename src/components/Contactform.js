66d7440b2d3638680b9c7e0036e4f656f006930e
Pravindu <pravindubh@gmail.com>
2023-07-06 01:24:36 +0530

    Add contact forn

--- src/components/Contactform.js
+++ src/components/Contactform.js
@@ -5,8 +5,8 @@ const ContactForm = () => {
   const [name,setName] = useState("");
   const [email,setEmail] = useState("");
   const [subject,setSubject] = useState("");
-  const [message,setMessage] = useState("");
-  console.log(name,email,subject,message);
+  // const [message,setMessage] = useState("");
+  console.log(name,email,subject);
   return (
     <div className="form">
       <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
@@ -18,14 +18,14 @@ const ContactForm = () => {
         <label>Subject</label>
         <input type="text" id="subject" name="subject" value={subject} onChange={(e) => setSubject(e.target.value)} required />
         <label>Message</label>
-        <input
-          // rows="6"
+        <textarea
+          rows="6"
           type="text"
-          // placeholder="Type Your Message Here"
+          placeholder="Type Your Message Here"
           id="message"
           name="message"
-          value={message} onChange={(e) => setMessage(e.target.value)}
-        required/>
+          // value={message} onChange={(e) => setMessage(e.target.value)}
+        />
         <button className="btn" type="submit">
           Submit
         </button>
