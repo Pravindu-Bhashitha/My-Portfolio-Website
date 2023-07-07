e99d552c6e5326440abc9a490943aa97f19e9124
Pravindu <pravindubh@gmail.com>
2023-07-07 09:15:47 +0530

    After adding use states to the form

--- src/components/Contactform.js
+++ src/components/Contactform.js
@@ -33,28 +33,35 @@
 // };
 
 // export default ContactForm;
-// import React, { useState,useRef } from "react";
+import { useState } from "react";
 import React, { useRef } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import "./FormStyles.css";
-import { toast,ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ContactForm = () => {
-  // const [name,setName] = useState("");
-  // const [email,setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   // const [subject,setSubject] = useState("");
-  // const [message,setMessage] = useState("");
+  const [message, setMessage] = useState("");
   // console.log(name,email,subject);
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_tcn0v0i', 'template_l5p8rpv', form.current, 'YgtBeZpyGjuFWP3bc')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_tcn0v0i",
+        "template_l5p8rpv",
+        form.current,
+        "YgtBeZpyGjuFWP3bc"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-          toast.success('Email Sent Successfully!', {
+          toast.success("Email Sent Successfully!", {
             position: "top-center",
             autoClose: 5000,
             hideProgressBar: false,
@@ -63,27 +70,47 @@ const ContactForm = () => {
             draggable: true,
             progress: undefined,
             theme: "dark",
-            });
-      }, (error) => {
+          });
+          setName("");
+          setEmail("");
+          setMessage("");
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
   return (
-
     <div className="form">
       <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" required/>
-      <label>Email</label>
-      <input type="email" name="user_email" required/>
-      <label>Message</label>
-      <textarea name="message" rows="6"/>
-      <input type="submit" value="Send" className="btn"/>
-      <ToastContainer/>
-    </form>
+        <label>Name</label>
+        <input
+          type="text"
+          name="user_name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          name="user_email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label>Message</label>
+        <textarea
+          name="message"
+          rows="6"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <input type="submit" value="Send" className="btn" />
+        <ToastContainer />
+      </form>
     </div>
   );
 };
 
 export default ContactForm;
-
